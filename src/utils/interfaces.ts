05004ba0c743b31a8ed9f998f29a83789230937a
Gabriel Gama <gafityr@gmail.com>
2026-08-20 15:27:17 -0300

    feat: create interfaces

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,18 @@
+export interface WeatherApiResponse {
+  latitude: number;
+  longitude: number;
+
+  current: {
+    temperature_2m: number;
+    relative_humidity_2m: number;
+    wind_speed_10m: number;
+    apparent_temperature: number;
+  };
+}
+
+export interface WeatherData {
+  temperature: number;
+  apparentTemperature: number;
+  humidity: number;
+  wind: number;
+}
