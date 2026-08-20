fc11e651fee94b3abfa5469452f4e253debb2d93
Gabriel Gama <gafityr@gmail.com>
2026-08-20 15:15:08 -0300

    feat: create fortaleza components

---
+++ src/fortaleza/fortaleza.tsx
@@ -0,0 +1,55 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import type { LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
+import {
+  NeighborhoodPolyline,
+  ClickableNeighborhoodPolygon,
+} from "../utils/neighborhood_polyline.tsx";
+
+import { useState } from "react";
+import type { WeatherData } from "@/utils/inferfaces.ts";
+import { WeatherStatusCard } from "@/components/weather_status_card.tsx";
+
+const FORTALPOSITION: LatLngExpression = [-3.795, -38.5266];
+
+export const Fortaleza = () => {
+  const [response, setResponse] = useState<WeatherData | null>(null);
+
+  return (
+    <div id="fortaleza" className="min-h-screen grid place-items-center">
+      <WeatherStatusCard weather={response} />
+      <MapContainer
+        center={FORTALPOSITION}
+        zoom={12}
+        scrollWheelZoom={true}
+        style={{ height: 610, width: 800 }}
+        className="relative p-8 z-10 items-center justify-center"
+      >
+        <TileLayer
+          attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ"
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+        />
+        <Marker
+          position={FORTALPOSITION}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+
+        <NeighborhoodPolyline />
+
+        <ClickableNeighborhoodPolygon setResponse={setResponse} />
+      </MapContainer>
+    </div>
+  );
+};
