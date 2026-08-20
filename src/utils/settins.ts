c38448e23f45f7e38a718351f7e0b2f3de188afc
Gabriel Gama <gafityr@gmail.com>
2026-08-20 15:27:58 -0300

    feat: create settins

---
+++ src/utils/settins.ts
@@ -0,0 +1,9 @@
+const checkAPI = () => {
+  const apiUrl = process.env.BUN_PUBLIC_API_URL;
+  if (!apiUrl) {
+    throw new Error(`Api aren't configured.`);
+  }
+  return apiUrl;
+};
+
+export const apiUrl = checkAPI();
