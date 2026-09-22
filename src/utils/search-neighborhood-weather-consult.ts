8a831bd8744dc705bc1fbad16ba591dfa587635a
Gabriel Gama <gafityr@gmail.com>
2026-09-21 22:50:04 -0300

    feat: create function parseNeighborhoodCoordinatesToString

--- src/utils/search-neighborhood-weather-consult.ts
+++ src/utils/search-neighborhood-weather-consult.ts
@@ -1,16 +1,27 @@
-import { weatherConsult as NeighborhoodWeatherConsult } from "@/utils/request-weather-api";
+import { consultNeighborhoodWeather } from "@/utils/request-weather-api";
+import { getNeighborhoodCoordinates } from "./coordinatesByNeighborhood";
 
-export async function searchNeighborhoodWeather(nameNeighborhood: string) {
-  switch (nameNeighborhood) {
-    case "Messejana":
-      const coordinates: { [coordinates: string]: string } = {
-        latitude: "-3.738639",
-        longitude: "-38.535288",
-      };
-      return await NeighborhoodWeatherConsult(coordinates);
+function parseNeighborhoodCoordinatesToString(neighborhood: string): {
+  [key: string]: string;
+} {
+  const neighborhoodLatLng: number[] = getNeighborhoodCoordinates(
+    neighborhood,
+  ) as number[];
+  const neighborhoodString = {
+    latitude: neighborhoodLatLng[0]!.toString(),
+    longitude: neighborhoodLatLng[1]!.toString(),
+  };
 
-    default:
-      console.log("No option selected");
-      alert("No data yet");
+  return neighborhoodString;
+}
+
+export async function getWeatherNeighborhood(neighborhood: string) {
+  const neighborhoodCoordinates =
+    parseNeighborhoodCoordinatesToString(neighborhood);
+
+  try {
+    return await consultNeighborhoodWeather(neighborhoodCoordinates);
+  } catch {
+    alert("No data yet.");
   }
 }
