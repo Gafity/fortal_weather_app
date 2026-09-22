fefd72917ed52d87004bc1cc43d9748e88f6510b
Gabriel Gama <gafityr@gmail.com>
2026-09-21 22:27:44 -0300

    refacto: move neighborhood.tsx to components folder as fortaleza-neighborhood.tsx

--- src/components/fortaleza-neighborhood.tsx
+++ src/components/fortaleza-neighborhood.tsx
@@ -1,22 +1,21 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import markerIconPng from "leaflet/dist/images/marker-icon.png";
-import { Icon } from "leaflet";
 import { NeighborhoodPolyline } from "../components/neighborhood-polyline.tsx";
-import { NeighborhoodButton } from "@/components/neighborhood-request-button.tsx";
+import { NeighborhoodPolygonButton } from "@/components/neighborhood-request-button.tsx";
 import "../../styles/index.css";
 
 import { useState } from "react";
-import type { WeatherData } from "@/utils/interfaces.ts";
+import type { WeatherData } from "@/utils/weather-types.ts";
 
 import { WeatherStatusCard } from "@/components/weather-status-card.tsx";
 
 const FORTALPOSITION: LatLngExpression = [-3.795, -38.5266];
 
-export const Fortaleza = () => {
+export const FortalezaNeighborhood = () => {
   const [weatherDataResponse, setWeatherDataResponse] =
     useState<WeatherData | null>(null);
+  const [neighborhoodName, setNeighborhoodName] = useState<string>();
   return (
     <div
       id="fortaleza"
@@ -33,26 +32,18 @@ export const Fortaleza = () => {
           attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ"
           url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
         />
-        <Marker
-          position={FORTALPOSITION}
-          icon={
-            new Icon({
-              iconUrl: markerIconPng,
-              iconSize: [25, 41],
-              iconAnchor: [12, 41],
-            })
-          }
-        >
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
 
         <NeighborhoodPolyline />
 
-        <NeighborhoodButton click={setWeatherDataResponse} />
+        <NeighborhoodPolygonButton
+          click={setWeatherDataResponse}
+          setNeighborhoodName={setNeighborhoodName}
+        />
       </MapContainer>
-      <WeatherStatusCard weather={weatherDataResponse} />
+      <WeatherStatusCard
+        weather={weatherDataResponse}
+        neiborhood={neighborhoodName}
+      />
     </div>
   );
 };
